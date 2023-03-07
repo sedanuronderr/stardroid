c059694c096497b6f178d9d6cc944373e51595d9
Seda Nur Önder <sednrondr@gmail.com>
2023-03-07 19:51:26 +0300

    ui change

--- app/src/main/java/com/google/android/stardroid/activities/DynamicStarMapActivity.java
+++ app/src/main/java/com/google/android/stardroid/activities/DynamicStarMapActivity.java
@@ -690,16 +690,6 @@ public class DynamicStarMapActivity extends InjectableActivity
 
   private void listDevice() {
 
-    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-      // TODO: Consider calling
-      //    ActivityCompat#requestPermissions
-      // here to request the missing permissions, and then overriding
-      //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-      //                                          int[] grantResults)
-      // to handle the case where the user grants the permission. See the documentation
-      // for ActivityCompat#requestPermissions for more details.
-      return;
-    }
     pairedDevice = myBluetoothAdapter.getBondedDevices();
 
 
